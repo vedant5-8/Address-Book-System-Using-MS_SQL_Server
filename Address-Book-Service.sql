512b86b66e0881889d611c44c60f757ed6dc5f24
Vedant <vedantnp200@gmail.com>
2023-06-29 10:53:35 +0530

    [Vedant]-Create new AddressBook table

--- Address-Book-Service.sql
+++ Address-Book-Service.sql
@@ -3,3 +3,17 @@
 CREATE DATABASE AddressBookService;
 
 USE AddressBookService;
+
+--UC 2: Create new table Address_Book in AddressBookService
+
+CREATE TABLE Address_Book(
+	First_Name VARCHAR(100),
+	Last_Name VARCHAR(100),
+	PhoneNo BIGINT,
+	EmailID VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(50),
+	State VARCHAR(50),
+	ZipCode INT
+);
+
