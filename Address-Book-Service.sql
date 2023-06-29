5739e937fe4cbfe8e8c4d230c1d1c5d6c574f8cc
Vedant <vedantnp200@gmail.com>
2023-06-29 11:39:41 +0530

    [Vedant]-Sort contacts in ascending order by First name for a particular city

--- Address-Book-Service.sql
+++ Address-Book-Service.sql
@@ -56,3 +56,9 @@ WHERE City = 'Alibag';
 
 SELECT COUNT(State) as Total_Contact FROM Address_Book
 WHERE State = 'Maharashtra';
+
+--UC8: retrieve entries sorted alphabetically by Person’s name for a given city
+
+SELECT * FROM Address_Book
+WHERE City = 'Alibag'
+ORDER BY First_Name;
