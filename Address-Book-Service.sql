effa9afe3bb4c36c321ce90797a07b1b0be8ae91
Vedant <vedantnp200@gmail.com>
2023-07-03 15:36:02 +0530

    [Vedant]-Insert some contacts in Contacts details table

--- Address-Book-Service.sql
+++ Address-Book-Service.sql
@@ -16,3 +16,14 @@ CREATE TABLE Contact_Details(
 	State VARCHAR(50),
 	ZipCode INT
 );
+
+--UC3: Insert records in Address Book Table
+
+INSERT INTO Address_Book VALUES
+	('Sahil', 'Mhatre', 8520025878, 'sahil@gmail.com', 'Chinchwali', 'Alibag', 'Maharashtra', 402209),
+	('Raj', 'Patil', 7539510325, 'rajpatil@gmail.com', 'Ranjankhar', 'Alibag', 'Maharashtra', 402209),
+	('Shubham', 'Patil', 8523697415, 'shubham@gmail.com', 'Hashiware', 'Alibag', 'Maharashtra', 402209),
+	('Mandar', 'Gavand', 8624851357, 'mandarg12@gmail.com', 'Waghran', 'Alibag', 'Maharashtra', 402209),
+	('Kunal', 'Patil', 7595351505, 'kunal@gmail.com', 'Pimpri', 'Pune', 'Maharashtra', 411017),
+	('Pratik', 'Thakur', 7898456512, 'pratik@gmail.com', 'vimannagar', 'Pune', 'Maharashtra', 411014),
+	('Omkar', 'Pawar', 8623598741, 'op29@gmail.com', 'Seawoods', 'Navi Mumbai', 'Maharashtra', 400706);
