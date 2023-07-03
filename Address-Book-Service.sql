8a35088323fcd84dd251efb16626dba7956fee11
Vedant <vedantnp200@gmail.com>
2023-07-03 15:32:24 +0530

    [Vedant]-Create new table in AddressBookService database

--- Address-Book-Service.sql
+++ Address-Book-Service.sql
@@ -4,3 +4,15 @@ CREATE DATABASE AddressBookService;
 
 USE AddressBookService;
 
+--UC 2: Create new table Contact Details in AddressBookService
+
+CREATE TABLE Contact_Details(
+	First_Name VARCHAR(100),
+	Last_Name VARCHAR(100),
+	PhoneNo BIGINT,
+	EmailID VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(50),
+	State VARCHAR(50),
+	ZipCode INT
+);
